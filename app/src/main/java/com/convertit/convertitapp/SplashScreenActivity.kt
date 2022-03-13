fd0feee07f9a609a2b6665f262c728bc69c0724b
Gabriel Bizarria <gabrielbizarriadev2@gmail.com>
2022-03-13 14:56:02 -0300

    Add: splash screen

---
+++ app/src/main/java/com/convertit/convertitapp/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.convertit.convertitapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.convertit.convertitapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convertitLogo.alpha = 0f
+
+
+        binding.convertitLogo.animate().setDuration(1500).alpha(1f).withEndAction{
+            Thread.sleep(1500)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
